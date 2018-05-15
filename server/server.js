98285d770f072cb5f02858111973f455a4a70d00
jaymeramsay <jayme.ramsay@gmail.com>
2018-05-15 18:46:48 -0500

    adds routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const methodOverride = require('method-override')
+// const methodOverride = require('method-override')
 const PORT = process.env.PORT || 7070;
 const app = express();
 
@@ -7,9 +7,13 @@ const bodyParser = require('body-parser');
 // const cookieParser = require('cookie-parser');
 // const session = require('cookie-session');
 
+const bands = require('./routes/routes.bands');
+const houses = require('./routes/routes.houses');
+const users = require('./routes/routes.users');
+
 
 // http method middleware for PATCH and DELETE
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 
 // forms handling
 app.use(bodyParser.urlencoded({
@@ -17,7 +21,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
+// routes
+app.use('/bands', bands);
+app.use('/houses', houses)
+app.use('/users', users);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
